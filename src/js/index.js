429df571a1a665a6498834756a57fce43093bb58
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-08-12 09:27:58 +0300

    add script

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import Notiflix from 'notiflix';
 import { fetchImages } from './pixabayApi';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import Simplelightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const searchFormEl = document.querySelector('.search-form');
 const galleryEl = document.querySelector('.gallery');
@@ -12,10 +12,12 @@ let simplelightbox;
 const perPage = 40;
 
 searchFormEl.addEventListener('submit', handleSearchForm);
+
 function renderGallery(images) {
-  if (!gallery) {
+  if (!galleryEl) {
     return;
   }
+
   const markup = images
     .map(image => {
       const {
@@ -28,9 +30,9 @@ function renderGallery(images) {
         comments,
         downloads,
       } = image;
-      return `<a class="gallery-link" href="${largeImageURL}">
+      return `<a class="gallery_link" href="${largeImageURL}">
 	<div class="photo-card" id="${id}">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <img class="gallery_img" src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
       <b>Likes</b>${likes}
@@ -49,5 +51,55 @@ function renderGallery(images) {
 </a>`;
     })
     .join('');
+  galleryEl.insertAdjacentHTML('beforeend', markup);
 }
-galleryEl.insertAdjacentHTML('beforeend', markup);
+
+function handleSearchForm(event) {
+  event.preventDefault();
+  page = 1;
+  query = event.currentTarget.elements.searchQuery.value.trim();
+  // console.log(query);
+  galleryEl.innerHTML = '';
+
+  if (query === '') {
+    Notiflix.Notify.failure('Please specify your search query');
+    return;
+  }
+
+  fetchImages(query, page, perPage)
+    .then(data => {
+      if (data.totalHits === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        renderGallery(data.hits);
+        simplelightbox = new Simplelightbox('.gallery a').refresh();
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      }
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      searchFormEl.reset();
+    });
+}
+
+function loadMoreImg() {
+  page += 1;
+  simplelightbox.destroy();
+
+  fetchImages(query, page, perPage)
+    .then(data => {
+      renderGallery(data.hits);
+      simplelightbox = new Simplelightbox('.gallery a').refresh();
+
+      const totalPage = Math.ceil(data.totalHits / perPage);
+
+      if (page > totalPage) {
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    })
+    .catch(error => console.log(error));
+}
